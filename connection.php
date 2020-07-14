8260860f586b2ff2d4ea484f859b88327d22a9b0
mharshita <manglaharshita@gmail.com>
2020-07-14 13:17:14 +0530

    second commit

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost","root","","credit_management");
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com","bf8744bff692bd","ca0d0251","heroku_1a383f032083827");
 ?>
